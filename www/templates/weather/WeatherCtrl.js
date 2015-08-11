00792d69470b3e635651e4d25b4d51e3b107cf8b
Jennifer Cho <jennifer.cho@udptechnology.com>
2015-08-11 11:08:56 +0900

    get weather info.

--- www/templates/weather/WeatherCtrl.js
+++ www/templates/weather/WeatherCtrl.js
@@ -1,23 +1,36 @@
 /**
- * Created by kimsuho on 15. 8. 8..
+ * Created by jodeng on 15. 8. 11..
  */
 (function(app){
     "use strict";
     app.controller('WeatherCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 
-        $scope.getWeather = function(location){
-            var apiUrl2 = 'http://api.openweathermap.org/data/2.5/weather?q='+location;
+        $scope.cityName = '';
+        $scope.countryName = '';
+        $scope.weatherList = [];
 
-            $http.get(apiUrl2).success(function(res){
-                console.info(res);
-                $scope.humidity = res.main.humidity;
+        var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast/city?id=1835848&APPID=4571c4074ee1d915ab33d052fbebdd59'
+        $scope.getWeatherInfo = function(){
+          $http.get(apiUrl).success(function(res){
+            console.info(res);
+            $scope.cityName = res.city.name;
+            $scope.countryName = res.city.country;
+            $scope.weatherList = res.list;
+          }).error(function(err){
+            console.warn(err);
+          });
+        }
 
-                $rootScope.$broadcast('weather', res.main.humidity);
-
-            }).error(function(err){
-                console.warn(err);
-            });
-
-        };
+        $scope.getWeaterInfoByCityName = function(cityName){
+          var searchUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+          $http.get(searchUrl).success(function(res){
+            console.info(res);
+            $scope.cityName = res.name;
+            $scope.humidity = res.main.humidity;
+            $rootScope.$broadcast('weather', res);
+          }).error(function(err){
+            console.warn(err);
+          });
+        }
     }]);
 }(app));
